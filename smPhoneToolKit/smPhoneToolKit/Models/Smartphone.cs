162aa81d076ffb364c9b0fe86c8b0ea009a27429
FreddyTheNut <f.erdnuess@gmail.com>
2018-06-02 20:55:08 +0200

    Added Smartphone model

---
+++ smPhoneToolKit/smPhoneToolKit/Models/Smartphone.cs
@@ -0,0 +1,42 @@
+﻿using smPhoneToolKit.Models.Enums;
+using smPhoneToolKit.ViewModels.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smPhoneToolKit.Models
+{
+    public class Smartphone : NotifyPropertyChanged
+    {
+        private string bezeichnung;
+        private Status status;
+
+        public string Bezeichnung
+        {
+            get { return this.bezeichnung; }
+            set
+            {
+                if(this.Bezeichnung != value)
+                {
+                    this.bezeichnung = value;
+                    OnPropertyChanged();
+                }
+            }     
+        }
+
+        public Status Status
+        {
+            get { return this.status; }
+            set
+            {
+                if(this.Status != value)
+                {
+                    this.status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
