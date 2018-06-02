76e2c3fe1860ba7937fb89ea26fed326cb0783c8
FreddyTheNut <f.erdnuess@gmail.com>
2018-06-02 16:57:47 +0200

    Add Helper for ViewModels

---
+++ smPhoneToolKit/smPhoneToolKit/ViewModels/Helper/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace smPhoneToolKit.ViewModels.Helper
+{
+    public class RelayCommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action method)
+        {
+            this.execute = new Action<object>(o => method());
+            this.canExecute = o => true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
