e4fa41bd83236f5f621da5cf42db8dd02bbb6e70
FreddyTheNut <f.erdnuess@gmail.com>
2018-06-02 20:29:05 +0200

    Add StatusDictionary dictionary

---
+++ smPhoneToolKit/smPhoneToolKit/Models/Dictionarys/StatusDictionary.cs
@@ -0,0 +1,28 @@
+﻿using smPhoneToolKit.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smPhoneToolKit.Models.Dictionarys
+{
+    public class StatusDictionary
+    {
+        private readonly Dictionary<String, Status> dic = new Dictionary<string, Status>();
+
+        public StatusDictionary()
+        {
+            this.dic.Add("unauthorized", Status.Unauthorized);
+            this.dic.Add("device", Status.ADB);
+            this.dic.Add("recovery", Status.Recovery);
+            this.dic.Add("sideload", Status.Sideload);
+            this.dic.Add("nodevice", Status.NoDevice);
+        }
+
+        public Dictionary<string, Status> GetStatusDictionary()
+        {
+            return this.dic;
+        }
+    }
+}
