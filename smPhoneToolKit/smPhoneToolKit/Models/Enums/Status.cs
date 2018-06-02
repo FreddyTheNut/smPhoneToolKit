02d4c51813437b541fa6302eb7e17bf9dbd494c2
FreddyTheNut <f.erdnuess@gmail.com>
2018-06-02 20:11:10 +0200

    Add Status enum

---
+++ smPhoneToolKit/smPhoneToolKit/Models/Enums/Status.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smPhoneToolKit.Models.Enums
+{
+    public enum Status
+    {
+        NoDevice,
+        Unauthorized,
+        ADB,
+        Root,       // + ADB
+        Recovery,    // + Root
+        Sideload,
+
+        //Just for Converters!
+        RootXRecovery,
+        RootXRecoveryXSideload,
+        RecoveryXSideload
+    }
+}
