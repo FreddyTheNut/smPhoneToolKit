13e8016b2bad89a3cad1dce82e2bdb80686d7f51
FreddyTheNut <f.erdnuess@gmail.com>
2018-06-02 23:05:49 +0200

    Add MainWindowViewModel

--- smPhoneToolKit/smPhoneToolKit/ViewModels/ViewModelLocator.cs
+++ smPhoneToolKit/smPhoneToolKit/ViewModels/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using smPhoneToolKit.ViewModels.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace smPhoneToolKit.ViewModels
 {
     public sealed class ViewModelLocator
     {
-        
+        private static readonly MainWindowViewModel mainWindow = new MainWindowViewModel();
+
+        public static MainWindowViewModel MainWindow
+        {
+            get
+            {
+                return ViewModelLocator.mainWindow;
+            }
+        }
     }
 }
---
+++ smPhoneToolKit/smPhoneToolKit/ViewModels/Views/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smPhoneToolKit.ViewModels.Views
+{
+    public class MainWindowViewModel
+    {
+    }
+}
